e3f810d552e3a63f39e86af30f25c1ba3179b4e8
Geordon Worley <vadixidav@gmail.com>
2017-02-27 12:30:58 -0500

    flesh out tests more

--- __main__.py
+++ __main__.py
@@ -8,11 +8,18 @@ from jsoncompare import jsoncompare
 if __name__ == '__main__':
     address = str(sys.argv[1])
     test_data = {'testint': 3, 'teststring': 'hi', 'testobj': {'inside': True}}
+    assert requests.get('http://' + address + '/v1/').status_code == 200
     response = requests.post('http://' + address + '/v1/')
     assert response.status_code == 200
     uid = response.json()['uid']
     response = requests.put('http://' + address + '/v1/' + str(uid), json=test_data)
     assert response.status_code == 200
+    response = requests.post('http://' + address + '/v1/' + str(uid), json=test_data)
+    assert response.status_code == 200
     response = requests.get('http://' + address + '/v1/' + str(uid))
     assert response.status_code == 200
     assert jsoncompare.are_same(test_data, response.json())
+    response = requests.delete('http://' + address + '/v1/' + str(uid))
+    assert response.status_code == 200
+    response = requests.get('http://' + address + '/v1/' + str(uid))
+    assert response.status_code == 404
