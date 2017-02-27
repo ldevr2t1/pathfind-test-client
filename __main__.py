736ebe9642d73e8b053f6005d55c4828d164a71d
Geordon Worley <vadixidav@gmail.com>
2017-02-27 13:54:32 -0500

    add test for invalid json

--- __main__.py
+++ __main__.py
@@ -7,10 +7,13 @@ from jsoncompare import jsoncompare
 if __name__ == '__main__':
     address = str(sys.argv[1])
     test_data = {'testint': 3, 'teststring': 'hi', 'testobj': {'inside': True}}
+    bad_data = '}}}}}}}}}}'
     assert requests.get('http://' + address + '/v1/').status_code == 200
     response = requests.post('http://' + address + '/v1/')
     assert response.status_code == 200
     uid = response.json()['uid']
+    response = requests.put('http://' + address + '/v1/' + str(uid), data=bad_data)
+    assert response.status_code == 500
     response = requests.put('http://' + address + '/v1/' + str(uid), json=test_data)
     assert response.status_code == 200
     response = requests.post('http://' + address + '/v1/' + str(uid), json=test_data)
