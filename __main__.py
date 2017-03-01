6f98e711fa5b3c5140d0354d6fb410c495334c57
Geordon Worley <vadixidav@gmail.com>
2017-03-01 11:06:20 -0500

    add commentary for tests and remove jsoncompare

--- __main__.py
+++ __main__.py
@@ -2,61 +2,93 @@
 
 import requests
 import sys
-from jsoncompare import jsoncompare
 
 def check_requests(requests, uid, expected):
     response = requests.get('http://' + address + '/v1/' + str(uid))
     assert response.status_code == 200
-    assert jsoncompare.are_same(expected, response.json())
+    assert expected == response.json()
 
 if __name__ == '__main__':
     address = str(sys.argv[1])
     test_data = {'testint': 3, 'teststring': 'hi', 'testobj': {'inside': True}}
     bad_data = '}}}}}}}}}}'
 
-    # Make sure the get request works on the root.
+    print("")
+    print("Make sure the get request works on the root.")
     assert requests.get('http://' + address + '/v1/').status_code == 200
 
-    # Make sure getting from a non-existing uid doesn't work.
+    print("")
+    print("Make sure getting from a non-existing uid doesn't work.")
     assert requests.get('http://' + address + '/v1/' + 'badtest').status_code == 404
 
-    # Attempt to post to the root to create a UID
+    print("")
+    print("Attempt to post to the root to create a UID.")
     response = requests.post('http://' + address + '/v1/')
     assert response.status_code == 200
     uid = response.json()['uid']
+    print("Success.")
 
-    # Attempt to put non-JSON data to the UID, expecting failure.
+    print("")
+    print("Attempt to put non-JSON data to the UID, expecting failure.")
     assert requests.put('http://' + address + '/v1/' + str(uid), data=bad_data).status_code == 400
+    print("Success.")
 
-    # Put legitimate JSON data into the provided UID.
+    print("")
+    print("Put legitimate JSON data into the provided UID.")
     assert requests.put('http://' + address + '/v1/' + str(uid), json=test_data).status_code == 200
-    # Ensure it was updated correctly.
+    print("Success.")
+
+    print("")
+    print("Ensure it was updated correctly.")
     check_requests(requests, uid, test_data)
+    print("Success.")
 
-    # Delete the UID before testing post.
+    print("")
+    print("Delete the UID before testing post.")
     assert requests.delete('http://' + address + '/v1/' + str(uid)).status_code == 200
+    print("Success.")
 
-    # Attempt to post to a deleted UID.
+    print("")
+    print("Attempt to post to a deleted UID.")
     assert requests.post('http://' + address + '/v1/' + str(uid), json=test_data).status_code == 404
+    print("Success.")
 
-    # Attempt to post to the root to create a UID
+    print("")
+    print("Attempt to post to the root to create a UID.")
     response = requests.post('http://' + address + '/v1/')
     assert response.status_code == 200
     uid = response.json()['uid']
+    print("Success.")
 
-    # Attempt to post non-JSON data to the UID, expecting failure.
+    print("")
+    print("Attempt to post non-JSON data to the UID, expecting failure.")
     assert requests.post('http://' + address + '/v1/' + str(uid), data=bad_data).status_code == 400
+    print("Success.")
 
-    # Post legitimate JSON data into the provided UID.
+    print("")
+    print("Post legitimate JSON data into the provided UID.")
     assert requests.post('http://' + address + '/v1/' + str(uid), json=test_data).status_code == 200
-    # Ensure it was updated correctly.
+    print("Success.")
+
+    print("")
+    print("Ensure it was updated correctly.")
     check_requests(requests, uid, test_data)
+    print("Success.")
 
-    # Delete the POST test UID.
+    print("")
+    print("Delete the POST test UID.")
     assert requests.delete('http://' + address + '/v1/' + str(uid)).status_code == 200
+    print("Success.")
 
-    # Try to delete a non-existing UID.
+    print("")
+    print("Try to delete a non-existing UID.")
     assert requests.delete('http://' + address + '/v1/' + str(uid)).status_code == 404
+    print("Success.")
 
-    # Attempt to get from a non-existing UID that existed before.
+    print("")
+    print("Attempt to get from a non-existing UID that existed before.")
     assert requests.get('http://' + address + '/v1/' + str(uid)).status_code == 404
+    print("Success.")
+
+    print("")
+    print("All tests passed.")
